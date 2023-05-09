c6c7ab4ddb17499b834f15a5ab9acea89f949b3d
Kamalesh Sriram Ganapathy <k.s.ganapathy@student.tudelft.nl>
2023-05-09 15:25:47 +0200

    updated propulsion.py

--- ADCS_n_Propulsion/proplusion.py
+++ ADCS_n_Propulsion/proplusion.py
@@ -11,7 +11,9 @@ A_extended = 0.330*(0.350+0.368)/2+0.220*0.350
 A_cube = 0.350*0.340
 Cd = 2.2
 m_dry = 54
+l = 2.7 # length of S/C
 T_life = 5*365*86400 # 5 years lifetime
+rho_p = 867 # Density of Propellant
 
 v = np.sqrt(mu/r)
 F_d = Cd*0.5*rho*v**2*A_extended # force due to drag
@@ -19,7 +21,7 @@ J = F_d*T_life # Total Impulse
 
 # Chosen parameters
 F_t = 0.03
-Isp = 230
+Isp = 1000
 
 print(f"The aerodynamic drag is {np.round(F_d, 6)} N")
 print(f"The total impulse required is {np.round(J, 3)} N s")
@@ -34,6 +36,9 @@ m_wet = m_s + m_p # wet mass of just propulsion system
 m_ratio = m_wet / m_dry # Mass ratio
 
 #Volume of Propulsion System
-V_s = 0.016
+V_p = m_p / rho_p # Volume of propellant
+V_s = 0.016 # Volume of structure
+V_wet = V_s + V_p
+V_ratio = V_wet / l**3 # Volume ratio
 
 print(m_p, m_wet)
