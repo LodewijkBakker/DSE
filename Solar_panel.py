4b41cdd57d2a7609371732dc8e2bc9de855fca92
Boyan Kolev <b.i.kolev@student.tudelft.nl>
2023-06-05 11:06:45 +0200

    push

--- Solar_panel.py
+++ Solar_panel.py
@@ -36,6 +36,14 @@ class Solar_panel_sizing:
         self.T_o = self.c_cst["Orbital"]["T_orbit"]
         self.T_e = self.T_o * self.c_cst["Orbital"]["perc_eclipse"]
         self.T_s = self.T_o - self.T_e
+        
+        self.S_flux = self.c_cst["Orbital"]["S_flux"]
+        self.Id = self.c_cst["Cell efficiency"]["Inherent_degradation"]
+
+        self.Cell_d = self.c_cst["Cell efficiency"]["cell_degradation"]
+        self.L = self.c_cst["Orbital"]["Mission_life"]
+        
+        self.Cell_D = (1-self.Cell_d) ** self.L
 
         # Import EPS parameters:
         self.n_pcdu = self.c_cst["EPS efficiency"]["PCDU"]
@@ -67,6 +75,21 @@ class Solar_panel_sizing:
         self.T_prop_tab = self.c_cst["Power Subsystems"]["T_prop"] # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...], if above To -> use To
         
         self.EPS_P_tab = self.EPS_loss() + self.c_cst["Power Subsystems"]["EPS_P"] # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...] -> Peak = Average
+
+        # --- Define Solar Panel design orbit parameters --- # 
+        self.cos_a1 = 2/np.pi        # Incidence angle efficiency for the different orbits
+        self.cos_a2 = 0.99           # Incidence angle efficiency for the different orbits
+        self.cos_a3 = 0.99           # Incidence angle efficiency for the different orbits
+        self.cos_a4 = None           # Incidence angle efficiency for the different orbits
+        self.cos_a5 = None           # Incidence angle efficiency for the different orbits
+
+        # --- Define Solar Panel design parameters --- #
+        self.A_top_fold = None            # maximum area available for 1 fold in [m^2]
+        self.A_side_fold = None           # maximum area available for 1 fold in [m^2]
+        self.A_back_fold = None           # maximum area available for 1 fold in [m^2]
+
+        self.A_max_tot = self.A_top_fold + 2*3*self.A_side_fold + 3*self.A_back_fold
+
 
     pass
 
@@ -112,5 +135,68 @@ class Solar_panel_sizing:
             + self.COMMS_P[1] * self.T_COMMS + self.GNS_P[1] * self.T_GNS \
             + self.SM_P[1] * self.T_SM + self.TCS_P[1] * self.T_TCS + self.Payload_d[1] * self.T_pay_d
         
+        E_a = self.ADCS_P[0] * (self.T_o - self.T_ADCS) + self.SDH_P[0] * (self.T_o - self.T_SDH) \
+            + self.COMMS_P[0] * (self.T_o - self.T_COMMS) + self.GNS_P[0] * (self.T_o - self.T_GNS) \
+            + self.SM_P[0] * (self.T_o - self.T_SM) + self.TCS_P[0] * (self.T_o - self.T_TCS)
+        
+        # Adding this to the contribution of Prop & EPS
+
+        E_orb = np.zeros((len(self.Prop_tab), len(self.Prop_tab[0]))) # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...]
+
+        for i in range(len(E_orb)):
+            # Configuration I
+            E_orb[i][0] = E_a + E_p + self.Prop_tab[i][1] * self.T_prop_tab[i][0] \
+                        + (self.T_o - self.T_prop_tab[i][0]) * self.Prop_tab[i][0] \
+                        + self.EPS_P_tab[i][0] * self.T_o
+
+            # Configuration II
+            E_orb[i][1] = E_a + E_p + self.Prop_tab[i][1] * self.T_prop_tab[i][1] \
+                        + (self.T_o - self.T_prop_tab[i][1]) * self.Prop_tab[i][0] \
+                        + self.EPS_P_tab[i][1] * self.T_o
+        
+        return E_orb
+    
+    def P_area(self):
+        ''' Function that computes the needed power of the different sizing scenarios A1, A2, A3'''
+
+        E_orb = self.E_orbit()
+
+        P_area_1 = np.zeros((len(E_orb), len(E_orb[0]))) # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...]
+        P_area_2 = np.zeros((len(E_orb), len(E_orb[0]))) # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...]
+        P_area_3 = np.zeros((len(E_orb), len(E_orb[0]))) # [[ConfigI, ConfigII], [ConfigI, ConfigII], [ConfigI, ConfigII], ...]
+
+        for i in range(len(E_orb)):
+            for j in range(len(E_orb[0])):
+                P_area_1[i][j] = E_orb[i][j] / (self.T_o / 2)
+                P_area_2[i][j] = E_orb[i][j] / (self.T_s)
+                P_area_3[i][j] = E_orb[i][j] / (self.T_o)
+        
+        return P_area_1, P_area_2, P_area_3
+    
+    def Solar_Panel_size(self):
+        ''' Function that computes the needed solar panel size for each configuration and design orbits, 
+            Data output is saved in ./Outpits/Solar_Panels/file.xslx'''
+        
+        P_area_1, P_area_2, P_area_3 = self.P_area()
+
+        # --- Compute the needed solar panel size for each configuration and design orbits using a range of solar panel efficiencies --- #
+
+        n_cell = np.arange(0.16, 0.36, 0.002) # [efficiency of solar cells] 16 % -> 36 % with a step of 0.2 %
+    
+
+        A1 = np.zeros((len(P_area_1), len(P_area_1[0]), len(n_cell))) # [[ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], ...]
+        A2 = np.zeros((len(P_area_2), len(P_area_2[0]), len(n_cell))) # [[ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], ...]
+        A3 = np.zeros((len(P_area_3), len(P_area_3[0]), len(n_cell))) # [[ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], [ConfigI[cells], ConfigII[cells]], ...]
+
+        for i in range(len(P_area_1)):
+            for j in range(len(P_area_1[0])):
+                for k in range(len(n_cell)):
+                    A1[i][j][k] = P_area_1[i][j] / (n_cell[k] * self.S_flux * self.cos_a1 * self.Id * self.Cell_D)
+                    A2[i][j][k] = P_area_2[i][j] / (n_cell[k] * self.S_flux * self.cos_a2 * self.Id * self.Cell_D)
+                    A3[i][j][k] = P_area_3[i][j] / (n_cell[k] * self.S_flux * self.cos_a3 * self.Id * self.Cell_D)
+
+        # Computing the back, top, side area required for each configuration and design orbits
+
+        pass
 
 pass
--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 
 # ---------------------------------- Import Libraries ---------------------------------------------#
 from Battery_sizing import Battery_sizing
+from Solar_panel import Solar_panel_sizing
 
 Bat_s = Battery_sizing()
 Bat_s.Bat_sizing()
+
+SP_s = Solar_panel_sizing()
+SP_s.Solar_Panel_size()
 
