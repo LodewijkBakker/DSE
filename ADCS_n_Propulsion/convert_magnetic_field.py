99fb5a250d101baa6cf029d4e1f0fcb0f47d9ca4
LodewijkBakker <75375396+LodewijkBakker@users.noreply.github.com>
2023-06-14 19:26:24 +0200

    Magnetic field and configs

--- ADCS_n_Propulsion/convert_magnetic_field.py
+++ ADCS_n_Propulsion/convert_magnetic_field.py
@@ -46,12 +46,9 @@ x = np.interp(np.linspace(0, len(x), num=1000), np.arange(0, len(x)), x)
 y = np.interp(np.linspace(0, len(y), num=1000), np.arange(0, len(y)), y)
 z = np.interp(np.linspace(0, len(z), num=1000), np.arange(0, len(z)), z)
 
-
-
 #Turn arrays to absolute values
-yaw = np.absolute(x)
-roll = np.absolute(y)
-pitch = np.absolute(z)
+mag_field = np.array([np.absolute(x), np.absolute(y), np.absolute(z)])
+
 
 # fig, ax = plt.subplots(3)
 # ax[1].plot(roll)
@@ -59,158 +56,157 @@ pitch = np.absolute(z)
 # ax[0].plot(yaw)
 # plt.show()
 
-#torque for roll , pitch, yaw HAS TO BE POSITIVE
-entry_torque = np.array([1.06e-5, 8.73e-5, 5.4e-6])  # average over 3 orbits so including propulsion
+def avg_torque_calc():
+    # torque for roll , pitch, yaw HAS TO BE POSITIVE
+    entry_torque = np.array([1.75E-05, 8.22E-05, 2.58E-05])
+    # average over 3 orbits so including propulsion
+    entry_torque_straight_prop = np.array([1.75E-05, 3.51E-04, 2.58E-05])
+    # average over 3 orbits so including propulsion
 
-#### sizing dipole moment
-#Average Dipole Moment
-dip_moment = np.divide(entry_torque, 1e-9 * np.array([np.average(roll), np.average(pitch), np.average(yaw)]))
-print(dip_moment)
-# Different detumbling magn_torque creater
-non_conservative_entry_torque = entry_torque  # when fields don't cancel out like gravity torque
-detumbling_speed = 200/180*np.pi  # 200 deg/s
-t_orbits_detumbling = 40*5423  # time available for detumbling until deorbit
-i_sat = 1.4  # moment of inertia of the satellite
-det_torque = non_conservative_entry_torque + detumbling_speed/t_orbits_detumbling * i_sat
-dip_moment = np.divide(det_torque, 1e-9 * np.array([np.average(roll), np.average(pitch), np.average(yaw)]))
-print(dip_moment)
+    t_orbit = 5431  # time of an orbit
+    # new average
+    # entry_torque[1] = (entry_torque[1]*t_orbit*3 + (entry_torque_straight_prop[1]-entry_torque[1])*900)/(t_orbit*3) #
 
+    avg_torque = np.array([1.75E-05, 8.22E-05, 2.58E-05])
+    return avg_torque
 
-#avg torque per timestep
-magn_torque_roll = list(map(lambda entry: (1e-9 * entry) * dip_moment[0], roll))
-magn_torque_pitch = list(map(lambda entry: (1e-9 * entry) * dip_moment[1], pitch))
-magn_torque_yaw = list(map(lambda entry: (1e-9 * entry) * dip_moment[2], yaw))
+def sizing_dipole(mag_field, avg_torque, I_sat):
+    """
+    sizes the dipole of the magnetic torquer
+    :param mag_field: roll, pitch yaw magnetic field in [nanotesla]
+    :param avg_torque: roll, pitch, yaw average torque over orbit [Nm]
+    :param I_sat: moment of inertia of the satellite along body axis (x, y, z)
+    :return dip_moment: dipole moment in [A/m]
+    """
 
+    avg_mag_field = 1e-9 * np.average(mag_field, axis=1)  # in [tesla]
+    # Nominal operation dip_moment
+    dip_moment_nom = np.divide(avg_torque, avg_mag_field)
+    # print(dip_moment_nom, 'nominal operation dip moment')
 
-# print(np.average(magn_torque_yaw), 'mty')
-# print(np.average(magn_torque_roll))
-# print(np.average(magn_torque_pitch))
-# print(magn_torque_yaw)
-# print(magn_torque_roll)
-# print(magn_torque_pitch)
+    # detumbling dip_moment required
+    detumbling_speed = 300/180*np.pi  # 200 deg/s
+    t_orbits_detumbling = 40*5423  # time available for detumbling until deorbit
 
-#get given array a , b , c
-# new_torque = a - yaw , b-roll, c-pitch  
-entry_roll_torque_cont = np.ones(len(x))*entry_torque[0]
-entry_pitch_torque_cont = np.ones(len(y))*entry_torque[1]
-entry_yaw_torque_cont = np.ones(len(z))*entry_torque[2]
+    detumbling_torque = detumbling_speed/t_orbits_detumbling * I_sat
+    dip_moment_tumb = np.divide(detumbling_torque, avg_mag_field)
+    # print(dip_moment_tumb, 'if sized on detumbling')
 
-# plt.plot(magn_torque_roll)
-# plt.plot(magn_torque_pitch)
-# plt.plot(magn_torque_yaw)
-# plt.plot(entry_roll_torque_cont)
-# plt.plot(entry_pitch_torque_cont)
-# plt.plot(entry_yaw_torque_cont)
-# plt.show()
+    dip_moment = np.maximum(dip_moment_nom, dip_moment_tumb)
+    # print(dip_moment, 'Dip used for calculation')
 
-result_torque_left_roll = list(map(lambda b, roll: b - roll, entry_roll_torque_cont, magn_torque_roll))
-result_torque_left_pitch = list(map(lambda c, pitch: c - pitch, entry_pitch_torque_cont, magn_torque_pitch))
-result_torque_left_yaw = list(map(lambda a, yaw: a - yaw, entry_yaw_torque_cont, magn_torque_yaw))
-
-# plt.plot(entry_roll_torque_cont)
-# plt.plot(entry_pitch_torque_cont)
-# plt.plot(entry_yaw_torque_cont)
-# plt.plot(result_torque_left_roll)
-# plt.plot(result_torque_left_pitch)
-# plt.plot(result_torque_left_yaw)
-# plt.show()
-
-#Integration
-
-angular_momentum_roll = integrate.cumtrapz(result_torque_left_roll)
-angular_momentum_pitch = integrate.cumtrapz(result_torque_left_pitch)
-angular_momentum_yaw = integrate.cumtrapz(result_torque_left_yaw)
-
-# Find The peaks at minimum points under 0
-
-roll_peaks, _ = find_peaks(-angular_momentum_roll, height=0)
-pitch_peaks, _ = find_peaks(-angular_momentum_pitch, height=0)
-yaw_peaks, _ = find_peaks(-angular_momentum_yaw, height=0)
-
-# plt.plot(angular_momentum_roll)
-# plt.plot(roll_peaks, angular_momentum_roll[roll_peaks], "x")
-# plt.plot(np.zeros_like(angular_momentum_roll), "--", color="gray")
-# plt.show()
-
-# plt.plot(angular_momentum_pitch)
-# plt.plot(pitch_peaks, angular_momentum_pitch[pitch_peaks], "x")
-# plt.plot(np.zeros_like(angular_momentum_pitch), "--", color="gray")
-# plt.show()
-
-# plt.plot(angular_momentum_yaw)
-# plt.plot(yaw_peaks, angular_momentum_yaw[yaw_peaks], "x")
-# plt.plot(np.zeros_like(angular_momentum_yaw), "--", color="gray")
-# plt.show()
-
-# Add the positive slopes
-
-for i in range(0, len(roll_peaks)):  
-    # print(roll_peaks[i])
-    if angular_momentum_roll[roll_peaks[i]] < 0:
-        # print(angular_momentum_roll[roll_peaks[i]])
-        angular_momentum_roll[roll_peaks[i]:] -= angular_momentum_roll[roll_peaks[i]]
-    # ideally use representative length, 
-    # not pitch yaw or something specifically
-
-for i in range(0, len(pitch_peaks)):  
-    # print(pitch_peaks[i])
-    if angular_momentum_pitch[pitch_peaks[i]] < 0:
-        # print(angular_momentum_pitch[pitch_peaks[i]])
-        angular_momentum_pitch[pitch_peaks[i]:] -= angular_momentum_pitch[pitch_peaks[i]]
-    # ideally use representative length, 
-    # not pitch yaw or something specifically
-
-for i in range(0, len(yaw_peaks)):  
-    # print(yaw_peaks[i])
-    if angular_momentum_yaw[yaw_peaks[i]] < 0:
-        # print(angular_momentum_yaw[yaw_peaks[i]])
-        angular_momentum_yaw[yaw_peaks[i]:] -= angular_momentum_yaw[yaw_peaks[i]]
-    # ideally use representative length, 
-    # not pitch yaw or something specifically
+    return dip_moment
 
 
-#To ensure no negative values 
-
-modified_roll = []
-for i in range(len(angular_momentum_roll)):
-    if angular_momentum_roll[i] >= 0:
-        modified_roll.append(angular_momentum_roll[i])
-    else:
-        modified_roll.append(0)
-
-modified_pitch = []
-for i in range(len(angular_momentum_pitch)):
-    if angular_momentum_pitch[i] >= 0:
-        modified_pitch.append(angular_momentum_pitch[i])
-    else:
-        modified_pitch.append(0)
-
-modified_yaw = []
-for i in range(len(angular_momentum_yaw)):
-    if angular_momentum_yaw[i] >= 0:
-        modified_yaw.append(angular_momentum_yaw[i])
-    else:
-        modified_yaw.append(0)
-
-plt.plot(angular_momentum_roll, label="roll")
-plt.plot(modified_roll, label="mod_roll")
-leg = plt.legend(loc='upper right')
-plt.show()
-
-plt.plot(angular_momentum_pitch, label="pitch")
-plt.plot(modified_pitch, label="mod _pitch")
-leg = plt.legend(loc='upper right')
-plt.show()
-
-plt.plot(angular_momentum_yaw, label="yaw")
-plt.plot(modified_yaw, label="mod_yaw")
-leg = plt.legend(loc='upper right')
-plt.show()
-
-average_test = [np.average(angular_momentum_roll), np.average(angular_momentum_pitch), np.average(angular_momentum_yaw)]
+avg_torque = avg_torque_calc()
+I_sat = np.array([0.33, 0.42, 0.72])  # moment of inertia of the satellite
+dip_moment = sizing_dipole(mag_field, avg_torque, I_sat)
 
 
-print(np.max(angular_momentum_yaw)*1000, np.max(angular_momentum_roll)*1000, np.max(angular_momentum_pitch)*1000, 'Angular momentum yaw roll pitch mNms')
+def res_torques_calc(mag_field, avg_torque):
+    """
+    :param mag_field: roll, pitch yaw magnetic field in [nanotesla]
+    :param avg_torque: roll, pitch, yaw average torque over orbit [Nm]
+    :return res_torques: roll, pitch, yaw resultant torques over orbit with desaturation [Nm]
+    """
+    mag_torque = np.multiply(dip_moment, mag_field)
+    res_torques = avg_torque - mag_torque
+
+    # # Add window of propulsion at one point
+    # result_torque_left_pitch[400:415] += (entry_torque_straight_prop[1]-entry_torque[1])
+    return res_torques
+
+
+def integrate_torques(res_torques):
+    """
+    :param res_torques: roll, pitch, yaw resultant torques over orbit with desaturation [Nm]
+    :return:
+    """
+    # Integration
+    min_to_sec = 30  # half a minute to seconds
+    angular_momentum = integrate.cumtrapz(res_torques, axis=1)*min_to_sec  # min to sec
+
+    return angular_momentum
+
+def angular_momentum_realism_creator(angular_momentum):
+    """
+    :param angular_momentum: stored velocity of spacecraft in reaction wheels [Nms]
+    :return:
+    """
+    # Find The peaks at minimum points under 0
+    roll_peaks, _ = find_peaks(-angular_momentum[0], height=0)
+    pitch_peaks, _ = find_peaks(-angular_momentum[1], height=0)
+    yaw_peaks, _ = find_peaks(-angular_momentum[2], height=0)
+
+    # Add the positive slopes
+
+    for i in range(0, len(roll_peaks)):
+        # print(roll_peaks[i])
+        if angular_momentum[0][roll_peaks[i]] < 0:
+            # print(angular_momentum_roll[roll_peaks[i]])
+            angular_momentum[0][roll_peaks[i]:] -= angular_momentum[0][roll_peaks[i]]
+        # ideally use representative length,
+        # not pitch yaw or something specifically
+
+    for i in range(0, len(pitch_peaks)):
+        # print(pitch_peaks[i])
+        if angular_momentum[1][pitch_peaks[i]] < 0:
+            # print(angular_momentum_pitch[pitch_peaks[i]])
+            angular_momentum[1][pitch_peaks[i]:] -= angular_momentum[1][pitch_peaks[i]]
+        # ideally use representative length,
+        # not pitch yaw or something specifically
+
+    for i in range(0, len(yaw_peaks)):
+        # print(yaw_peaks[i])
+        if angular_momentum[2][yaw_peaks[i]] < 0:
+            # print(angular_momentum_yaw[yaw_peaks[i]])
+            angular_momentum[2][yaw_peaks[i]:] -= angular_momentum[2][yaw_peaks[i]]
+        # ideally use representative length,
+        # not pitch yaw or something specifically
+
+
+    #To ensure no negative values
+
+    modified_roll = []
+    for i in range(len(angular_momentum[0])):
+        if angular_momentum[0][i] >= 0:
+            modified_roll.append(angular_momentum[0][i])
+        else:
+            modified_roll.append(0)
+
+    modified_pitch = []
+    for i in range(len(angular_momentum[1])):
+        if angular_momentum[i][1] >= 0:
+            modified_pitch.append(angular_momentum[i][1])
+        else:
+            modified_pitch.append(0)
+
+    modified_yaw = []
+    for i in range(len(angular_momentum[2])):
+        if angular_momentum[i][2] >= 0:
+            modified_yaw.append(angular_momentum[i][2])
+        else:
+            modified_yaw.append(0)
+
+    plt.plot(angular_momentum[:][0], label="roll")
+    plt.plot(modified_roll, label="mod_roll")
+    leg = plt.legend(loc='upper right')
+    plt.show()
+
+    plt.plot(angular_momentum[:][1], label="pitch")
+    plt.plot(modified_pitch, label="mod _pitch")
+    leg = plt.legend(loc='upper right')
+    plt.show()
+
+    plt.plot(angular_momentum[:][2], label="yaw")
+    plt.plot(modified_yaw, label="mod_yaw")
+    leg = plt.legend(loc='upper right')
+    plt.show()
+
+    average_test = np.average(angular_momentum, axis=1)
+
+    safety_factor = 2
+    print(np.max(angular_momentum_roll)*1000*safety_factor, np.max(angular_momentum_pitch)*1000*safety_factor, np.max(angular_momentum_yaw)*1000*safety_factor, 'Angular momentum roll pitch yaw mNms, with safety factor 2')
 
 # print('average', np.average(x), np.average(y), np.average(z)) #nanotesla
 # print('max', np.max(x), np.max(y), np.max(z))
